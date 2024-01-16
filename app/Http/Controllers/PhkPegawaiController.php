ec181e09534e7ddbba9555374d50fef5b90ce2d0
farrelfavian18 <farrelfavian180502@mhs.mdp.ac.id>
2024-01-17 02:26:45 +0700

    Laravel-Setup
    Instalasi Laravel Breeze, Laravel Spatie(Belum), dan Migration(Belum Relasi)

---
+++ app/Http/Controllers/PhkPegawaiController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PhkPegawai;
+use Illuminate\Http\Request;
+
+class PhkPegawaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PhkPegawai $phkPegawai)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PhkPegawai $phkPegawai)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PhkPegawai $phkPegawai)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PhkPegawai $phkPegawai)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PromosiDemosiController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PromosiDemosi;
+use Illuminate\Http\Request;
+
+class PromosiDemosiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PromosiDemosi $promosiDemosi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PromosiDemosi $promosiDemosi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PromosiDemosi $promosiDemosi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PromosiDemosi $promosiDemosi)
+    {
+        //
+    }
+}
